d983ca370f0fd24af0e5a3a3a36fc8903c5010af
Alyssa KT <a.kwong-taylor@outlook.com>
2019-11-18 20:44:10 -0800

    Main HTML and CSS for site header added. Fixed references in _font.scss. ***Bug to be fixed: Issue with active link's border bottom causing logo to bounce at Tablet sizes and up.

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react'; 
+import logo from "../assets/Logo/Logo-instock.svg"; 
 
 export default function header() {
     return (
-        <div>
-            
+        <header className="header">
+            <div className="header__wrapper">
+                <a href="/"><img className="header__logo" src={logo} alt="InStock logo" /></a>
+                    <nav className="header__nav">
+                        <ul className="header__nav-menu">
+                            <a className="header__nav-link" href="/">
+                                <li className="header__nav-item">Inventory</li>
+                            </a>
+                            <a className="header__nav-link" href="/">
+                                <li className="header__nav-item">Locations</li>
+                            </a>
+                        </ul>
+                    </nav>
         </div>
+    </header>
     )
 }
 
