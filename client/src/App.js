0cdfed9734011ae7ea1227b547ac98b6f8fd693c
Codee Palmer <codeejpalmer@gmail.com>
2019-11-20 12:19:10 -0800

    mapping functions working and products added to json files

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./components/header";
 import Inventory from "./components/inventory";
 import Location from "./components/location";
+// import PopUpForm from "./components/PopUpForm";
 //import ProductHeader from "./components/inventory/ProductHeader";
 // import TableHeader from "./components/location/locationTableHeader";
 // import TableHeaderInventory from "./components/inventory/InventoryTableHeader";
@@ -24,6 +25,7 @@ function App() {
           }}
         />
         <Route path='/locations' component={Location} />
+        {/* <Route path='/inventory' component={PopUpForm} /> */}
       </Switch>
     </div>
   );
