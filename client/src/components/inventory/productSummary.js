41bedcb606971a1016dc1db01b3f44cb281b18a8
David Gold <david.the.mavid@gmail.com>
2019-11-20 13:52:48 -0800

    Created headers for Product Summary page

--- client/src/components/inventory/productSummary.js
+++ client/src/components/inventory/productSummary.js
@@ -1,5 +1,23 @@
 import React from "react";
 
-export default function productHeader() {
-  return <div></div>;
+export default function productSummary(props) {
+  console.log(props);
+  return (
+    <div className="product__summary">
+      <h4 className="product__summary-headings">ITEM DESCRIPTION</h4>
+      <h4 className="inventory__name"></h4>
+
+      <h4 className="product__summary-headings">ORDERED BY</h4>
+
+      <h4 className="product__summary-headings">REFERENCE NUMBER </h4>
+
+      <h4 className="product__summary-headings">LAST ORDERED</h4>
+
+      <h4 className="product__summary-headings">LOCATION</h4>
+
+      <h4 className="product__summary-headings">QUANITTY</h4>
+
+      <h4 className="product__summary-headings">CATEGORIES</h4>
+    </div>
+  );
 }
