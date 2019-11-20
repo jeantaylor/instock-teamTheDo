8ffe3084df48919abb5c4b7c87742ec45c1112c2
Codee Palmer <codeejpalmer@gmail.com>
2019-11-19 22:03:11 -0800

    Added companies to products in our JSON file, did some styling for locations and inventory page

--- client/src/components/inventory.js
+++ client/src/components/inventory.js
@@ -19,19 +19,25 @@ export default class inventory extends Component {
     });
   }
   render() {
-    console.log(this.state.inventory);
-
     let newInventory = this.state.inventory.map(item => {
       return (
-        <div className='inventory__item' key={item.ref}>
+       
+        <div className='inventory__item' key={item.products.ref}>
           <div className='inventory__group'>
-            <h2 className='inventory__name'>{item.name}</h2>
-            <h4 className='inventory__description'>{item.desc}</h4>
+          <h4 className='table__header-headings-map'>ITEM</h4>
+            <h2 className='inventory__name'>{item.products.name}</h2>
+            <h4 className='inventory__description'>{item.products.desc}</h4>
           </div>
-          <h4 className='inventory__lastOrdered'>{item.lastOrdered}</h4>
-          <h4 className='inventory__location'>{item.location}</h4>
-          <h4 className='inventory__quantity'>{item.quantity}</h4>
-          <h4 className='inventory__status'>{item.status}</h4>
+          <h4 className='table__header-headings-map'>LAST ORDERED</h4>
+          <h4 className='inventory__lastOrdered'>
+            {item.products.lastOrdered}
+          </h4>
+          <h4 className='table__header-headings-map'>LOCATION</h4>
+          <h4 className='inventory__location'>{item.products.location}</h4>
+          <h4 className='table__header-headings-map'>QUANTITY</h4>
+          <h4 className='inventory__quantity'>{item.products.quantity}</h4>
+          <h4 className='table__header-headings-map'>STATUS</h4>
+          <h4 className='inventory__status'>{item.products.status}</h4>
         </div>
       );
     });
