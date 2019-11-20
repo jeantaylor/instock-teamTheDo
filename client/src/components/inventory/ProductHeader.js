ad16bb3dce32fc586e0464faa5c81c41b8c524a1
David Gold <david.the.mavid@gmail.com>
2019-11-19 16:04:25 -0800

    Product Header styling completed.

--- client/src/components/inventory/ProductHeader.js
+++ client/src/components/inventory/ProductHeader.js
@@ -1,15 +1,20 @@
 import React from "react";
 import backArrow from "../../assets/Icons/SVG/Icon-back-arrow.svg";
 
-
 export default function productHeader() {
   return (
     <div className="product__header">
-        <div className="product__header__wrapper">
-      <img src={backArrow} alt="back arrow"/>
-      <h1 className="product__header">PRODUCT</h1>
+      <div className="product__header__wrapper">
+        <div className="product__header__title__wrapper">
+          <img
+            className="product__header__icon"
+            src={backArrow}
+            alt="back arrow"
+          />
+          <h1 className="product__header__title">Product Name</h1>
+        </div>
+        <button className="product__header__button">In Stock</button>
       </div>
-      <button className="product__instock__button">In Stock</button>
     </div>
   );
 }
