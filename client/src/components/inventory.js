e7bb2b00b306c8975da3099454d7c6b38e61d9e7
David Gold <david.the.mavid@gmail.com>
2019-11-20 11:58:35 -0800

    Inventory create button created & styled

--- client/src/components/inventory.js
+++ client/src/components/inventory.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TableHeader from "./inventory/inventoryTableHeader";
+import CreateButton from "./inventory/inventoryCreateButton";
 import axios from "axios";
 
 export default class inventory extends Component {
@@ -21,30 +22,34 @@ export default class inventory extends Component {
   render() {
     let newInventory = this.state.inventory.map(item => {
       return (
-        <div className='inventory__item' key={item.products.ref}>
-          <div className='inventory__group'>
-            <h4 className='table__header-headings-map'>ITEM</h4>
-            <h2 className='inventory__name'>{item.products.name}</h2>
-            <h4 className='inventory__description'>{item.products.desc}</h4>
+        <div className="inventory__item" key={item.products.ref}>
+          <div className="inventory__group">
+            <h4 className="table__header-headings-map">ITEM</h4>
+            <h2 className="inventory__name">{item.products.name}</h2>
+            <h4 className="inventory__description">{item.products.desc}</h4>
           </div>
-          <h4 className='table__header-headings-map'>LAST ORDERED</h4>
-          <h4 className='inventory__lastOrdered'>
+          <h4 className="table__header-headings-map">LAST ORDERED</h4>
+          <h4 className="inventory__lastOrdered">
             {item.products.lastOrdered}
           </h4>
-          <h4 className='table__header-headings-map'>LOCATION</h4>
-          <h4 className='inventory__location'>{item.products.location}</h4>
-          <h4 className='table__header-headings-map'>QUANTITY</h4>
-          <h4 className='inventory__quantity'>{item.products.quantity}</h4>
-          <h4 className='table__header-headings-map'>STATUS</h4>
-          <h4 className='inventory__status'>{item.products.status}</h4>
+          <h4 className="table__header-headings-map">LOCATION</h4>
+          <h4 className="inventory__location">{item.products.location}</h4>
+          <h4 className="table__header-headings-map">QUANTITY</h4>
+          <h4 className="inventory__quantity">{item.products.quantity}</h4>
+          <h4 className="table__header-headings-map">STATUS</h4>
+          <h4 className="inventory__status">{item.products.status}</h4>
         </div>
       );
     });
 
     return (
-      <div className='inventory'>
-        <h1 className='inventory__heading'>Inventory</h1>
+      <div className="inventory">
+        <CreateButton />
+
+        <h1 className="inventory__heading">Inventory</h1>
+
         <TableHeader />
+
         {newInventory}
       </div>
     );
--- client/src/components/inventory/productSummary.js
+++ client/src/components/inventory/productSummary.js
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function productHeader() {
-  return (
-    <div>
-      <h1 className="summary__header">Product Summary</h1>
-    </div>
-  );
+  return <div></div>;
 }
