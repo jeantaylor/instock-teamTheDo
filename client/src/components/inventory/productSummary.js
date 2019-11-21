61471d3dade58ac05e342ef25333cca734611b02
David Gold <david.the.mavid@gmail.com>
2019-11-21 10:53:11 -0800

    Commiting for a git pull

--- client/src/components/inventory/productSummary.js
+++ client/src/components/inventory/productSummary.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function productSummary(props) {
-  //console.log(props);
-  return (
-    <div className="product__summary">
-      <h4 className="product__summary-headings">ITEM DESCRIPTION</h4>
-      <h4 className="inventory__name"></h4>
+export default class ProductSummary extends Component {
+  componentDidMount() {
+    axios.get(`http://localhost:8080/inventory`).then(res => {
+      //console.log(res.data);
+      const inventoryProps =
+        props.data &&
+        props.datas.map(function(data, index) {
+          console.log(data);
 
-      <h4 className="product__summary-headings">ORDERED BY</h4>
+          render();
+          {
+            return (
+              <div className="product__summary">
+                <h4 className="product__summary-headings">ITEM DESCRIPTION</h4>
+                <h4 className="inventory__name">name</h4>
 
-      <h4 className="product__summary-headings">REFERENCE NUMBER </h4>
+                <h4 className="product__summary-headings">ORDERED BY</h4>
 
-      <h4 className="product__summary-headings">LAST ORDERED</h4>
+                <h4 className="product__summary-headings">REFERENCE NUMBER </h4>
 
-      <h4 className="product__summary-headings">LOCATION</h4>
+                <h4 className="product__summary-headings">LAST ORDERED</h4>
 
-      <h4 className="product__summary-headings">QUANITTY</h4>
+                <h4 className="product__summary-headings">LOCATION</h4>
 
-      <h4 className="product__summary-headings">CATEGORIES</h4>
-    </div>
-  );
+                <h4 className="product__summary-headings">QUANTITY</h4>
+
+                <h4 className="product__summary-headings">CATEGORIES</h4>
+              </div>
+            );
+          }
+        });
+    });
+  }
 }
