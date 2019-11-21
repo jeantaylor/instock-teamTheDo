25dd6e56b8cd8b5a4e987782355b5382d1da3624
David Gold <david.the.mavid@gmail.com>
2019-11-21 13:01:34 -0800

    Passed props to Product Summary & added get request to api

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,33 @@ import Header from "./components/header";
 import Inventory from "./components/inventory";
 import Location from "./components/location";
 import PopUpForm from "./components/PopUpForm";
+import ProductHeader from "./components/inventory/ProductHeader";
+import ProductSummary from "./components/inventory/productSummary";
 
-//import ProductHeader from "./components/inventory/ProductHeader";
 // import TableHeader from "./components/location/locationTableHeader";
 // import TableHeaderInventory from "./components/inventory/InventoryTableHeader";
 // import InventoryKbabMenu from "./components/inventory/InventoryKbabMenu";
-import ProductSummary from "./components/inventory/productSummary";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
+
       <Switch>
-        <Route exact path='/inventory' component={Inventory} />
+        <Route exact path="/inventory" component={Inventory} />
         <Route
-          path='/inventory/:ref'
+          path="/inventory/:ref"
           render={props => {
-            return <Inventory {...props} />;
+            return (
+              <>
+                <ProductHeader />
+                <ProductSummary />
+              </>
+            );
           }}
         />
-        <Route path='/locations' component={Location} />
-        <Route path='/createNew' component={PopUpForm} />
+        <Route path="/locations" component={Location} />
+        <Route path="/createNew" component={PopUpForm} />
       </Switch>
     </div>
   );
--- client/src/components/inventory/productSummary.js
+++ client/src/components/inventory/productSummary.js
@@ -2,36 +2,42 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class ProductSummary extends Component {
+  state = {
+    products: []
+  };
+
   componentDidMount() {
-    axios.get(`http://localhost:8080/inventory`).then(res => {
+    axios.get(`http://localhost:8080/inventory/:ref`).then(res => {
       //console.log(res.data);
-      const inventoryProps =
-        props.data &&
-        props.datas.map(function(data, index) {
-          console.log(data);
-
-          render();
-          {
-            return (
-              <div className="product__summary">
-                <h4 className="product__summary-headings">ITEM DESCRIPTION</h4>
-                <h4 className="inventory__name">name</h4>
-
-                <h4 className="product__summary-headings">ORDERED BY</h4>
-
-                <h4 className="product__summary-headings">REFERENCE NUMBER </h4>
-
-                <h4 className="product__summary-headings">LAST ORDERED</h4>
-
-                <h4 className="product__summary-headings">LOCATION</h4>
-
-                <h4 className="product__summary-headings">QUANTITY</h4>
-
-                <h4 className="product__summary-headings">CATEGORIES</h4>
-              </div>
-            );
-          }
-        });
+      this.setState({
+        products: res.data
+      });
     });
+  }
+  render() {
+    let inventoryProps =
+      this.state.data &&
+      this.state.map(product => {
+        return (
+          <div className="product__summary">
+            <h4 className="product__summary-headings">ITEM DESCRIPTION</h4>
+            <h4 className="inventory__name">{product.desc}</h4>
+            <h4 className="product__summary-headings">ORDERED BY</h4>
+            <h4 className="inventory__name">{product.name}</h4>
+            <h4 className="product__summary-headings">REFERENCE NUMBER </h4>
+            <h4 className="inventory__name">{product.id}</h4>
+            <h4 className="product__summary-headings">LAST ORDERED</h4>
+            <h4 className="inventory__name">{product.lastOrdered}</h4>
+            <h4 className="product__summary-headings">LOCATION</h4>
+            <h4 className="inventory__name">{product.location}</h4>
+            <h4 className="product__summary-headings">QUANTITY</h4>
+            <h4 className="inventory__name">{product.quantity}</h4>
+            <h4 className="product__summary-headings">CATEGORIES</h4>
+            <h4 className="inventory__name">{product.categories}</h4>
+          </div>
+        );
+      });
+    //console.log(inventoryProps);
+    return <div className="product__summary">{inventoryProps}</div>;
   }
 }
