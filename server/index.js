cc037fd269179ba15885250c187846a6583fd831
Alyssa KT <a.kwong-taylor@outlook.com>
2019-11-25 15:54:24 -0800

    Axios set up for editProduct page to load current info into form.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -10,21 +10,23 @@ app.use(express.json());
 
 /// Const routers
 // Template:
-const inventoryRouter = require("./routes/api/inventoryAPI");
-const inventoryRouterTwo = require("./routes/api/locationsApi");
+const inventoryRouter = require('./routes/api/inventoryAPI');
+const inventoryRouterTwo = require('./routes/api/locationsApi');
+const inventoryRouterThree = require("./routes/api/editAPI");
 
 /// Simple API Documentation
-app.get("/", (req, res) => {
-  res.json({
-    msg: "You can perform these requests from this API:"
-  });
+app.get('/', (req, res) => {
+	res.json({
+		msg: 'You can perform these requests from this API:'
+	});
 });
 
 /// Link Route Files
 // Template:
-app.use("/inventory", inventoryRouter);
-app.use("/locations", inventoryRouterTwo);
+app.use('/inventory', inventoryRouter);
+app.use('/locations', inventoryRouterTwo);
+app.use('/edit', inventoryRouterThree); 
 
 app.listen(PORT, () => {
-  console.log(`Now listening on port ${PORT}`);
+	console.log(`Now listening on port ${PORT}`);
 });
